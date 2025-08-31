950fb61b87334fd7507972099a260b3fa2f8f46d
GRC <geloryann.carbonell@gmail.com>
2025-08-31 17:09:03 +0800

    Add Jest tests and CI test job

--- src/__tests__/greet.test.js
+++ src/__tests__/greet.test.js
@@ -1,6 +1,5 @@
-const { greet } = require('../greet');//comment
+const { greet } = require('../greet');
 test('returns expected greeting', () => {
     expect(greet('Team')).toBe('Hello, Team! Welcome to Agile DevOps.');
 });
 
-asdasasdas
